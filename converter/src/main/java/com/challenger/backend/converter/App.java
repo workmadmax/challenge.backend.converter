c6209f5a423e0c5c35f300872b70fbe64936ef7f
mdouglas <work.madmax@gmail.com>
2023-09-04 16:36:01 -0300

    feat: pom xml add

---
+++ converter/src/main/java/com/challenger/backend/converter/App.java
@@ -0,0 +1,16 @@
+package com.challenger.backend.converter;
+
+import com.challenger.backend.converter.domain.libs.dialog_box.DialogBox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        DialogBox   dialogBox = new DialogBox();
+        dialogBox.execute();
+    }
+}
---
+++ converter/src/test/java/com/challenger/backend/converter/AppTest.java
@@ -0,0 +1,20 @@
+package com.challenger.backend.converter;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
