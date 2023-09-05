7cfb617c081df7a09ba9e5353f660f1082c4f061
mdouglas <work.madmax@gmail.com>
2023-09-05 16:35:52 -0300

    feat: .jar

--- converter/src/main/java/com/challenger/backend/converter/App.java
+++ converter/src/main/java/com/challenger/backend/converter/App.java
@@ -3,7 +3,8 @@ package com.challenger.backend.converter;
 import com.challenger.backend.converter.domain.libs.dialog_box.DialogBox;
 
 /**
- * Hello world!
+ * Uma caixa de dialogo que converte um valor em real para dolar, euro e libra.
+ * Ou pode converter temperatura de celsius para fahrenheit e vice-versa.
  *
  */
 public class App 
